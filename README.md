ed994658882c8e2f5bdb1e580f3abbd2debae501
Willem van Heemstra <willem@vanheemstrasystems.com>
2016-08-10 16:36:21 +0100

    Modified

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # angularjs1-5-route-resolve
 AngularJS 1.5 Route Resolve
+
+Based on 'Route Resolve' at https://www.youtube.com/watch?v=7BVo-72tgG0&list=PL6n9fhu94yhWKHkcL7RJmmXyxkuFB3KSl&index=41
+
+#Route Resolve
+
+The benefit of route resolve is that we won't transition to the new route until all promises have been resolved.
+
+Using the route ```resolve``` property on the '/students' route as follows
+
+Note: here we are imitating a network latency of 2000 milliseconds in the students controller
+
+```javascript
+    [...]
+    .when("/students", {
+        templateUrl: "templates/students.html",
+        controller: "studentsCtrl",
+        controllerAs: "ctrl",
+        resolve: {
+            // A function that returns a promise
+            studentsList: function($http) {
+                // return $http.get("StudentsService/GetAllStudents")
+                //      .then(function(response) {
+                //            return response.data;
+                //       }, function(reason) {
+                //            return reason.data;
+                //       });
+                /**
+                 * Delay for a number of milliseconds
+                 */
+                function sleep(delay) {
+                    var start = new Date().getTime();
+                    while (new Date().getTime() < start + delay);
+                }
+                // Imitate a network latency by 2000 milliseconds
+                sleep(2000);
+                vm.students = [{ id: 1, name: "Ben", gender: "Male", city: "London" }, { id: 2, name: "Matt", gender: "Male", city: "New York" }, { id: 3, name: "Pam", gender: "Female", city: "Chennai" }];
+            }
+        }
+    })
+```
+
+Then inject the newly created resolve function 'studentsList' into the students controller and assign the model vm.students to it.
+ 
+```javascript
+    .controller("studentsCtrl", function(studentsList, $route, $location) {
+
+        var vm = this;
+
+        vm.searchStudent = function() {
+            if(vm.name) {
+                $location.url("/studentsSearch/" + vm.name);
+            }
+            else {
+                $location.url("/studentsSearch");
+            }
+        }
+
+        vm.reloadData = function() {
+            $route.reload();
+        }
+
+        vm.students = studentsList;
+``` 
+
+In summary:
+
+1) The resolve property contains one or more promises that must resolve successfully before transitioning to the new route.
+
+2) The property names used in the resolve property can then be injected in the controller. This means the controller does not have to fetch the data.
+
+See script.js, index.html and styles.css how to implement this.
---
+++ script.js
@@ -0,0 +1,139 @@
+var myApp = angular
+    .module("myModule", ["ngRoute"])
+    .config(function($routeProvider) {
+        //$routeProvider.caseInsensitiveMatch = true;
+
+        $routeProvider
+            .when("/home", {
+                templateUrl: "templates/home.html",
+                controller: "homeCtrl",
+                controllerAs: "ctrl",
+                caseInsensitiveMatch: true
+            })
+            .when("/courses", {
+                templateUrl: "templates/courses.html",
+                controller: "coursesCtrl",
+                controllerAs: "ctrl"
+            })
+            .when("/students", {
+                templateUrl: "templates/students.html",
+                controller: "studentsCtrl",
+                controllerAs: "ctrl",
+                resolve: {
+                    // A function that returns a promise
+                    studentsList: function($http) {
+                        // return $http.get("StudentsService/GetAllStudents")
+                        //      .then(function(response) {
+                        //            return response.data;
+                        //       }, function(reason) {
+                        //            return reason.data;
+                        //       });
+                        /**
+                         * Delay for a number of milliseconds
+                         */
+                        function sleep(delay) {
+                            var start = new Date().getTime();
+                            while (new Date().getTime() < start + delay);
+                        }
+                        // Imitate a network latency by 2000 milliseconds
+                        sleep(2000);
+                        vm.students = [{ id: 1, name: "Ben", gender: "Male", city: "London" }, { id: 2, name: "Matt", gender: "Male", city: "New York" }, { id: 3, name: "Pam", gender: "Female", city: "Chennai" }];
+                    }
+                }
+            })
+            .when("/students/:id", {
+                templateUrl: "templates/studentDetails.html",
+                controller: "studentDetailsCtrl",
+                controllerAs: "ctrl"
+            })
+            .when("/studentsSearch/:name?", {
+                templateUrl: "templates/studentsSearch.html",
+                controller: "studentsSearchCtrl",
+                controllerAs: "ctrl"
+            })
+            .otherwise({
+                redirectTo: "/home"
+            })
+    })
+    .controller("homeCtrl", function() {
+        var vm = this;
+        vm.pageTitle = "Home";
+    })
+    .controller("coursesCtrl", function($http) {
+        var vm = this;
+        // $http.get("CourseService/GetAllCourses")
+        //      .then(function(response) {
+        //            vm.courses = response.data;
+        //       }, function(reason) {
+        //            vm.error = reason.data;
+        //       });
+        vm.courses = [{ id: 1, name: "Ruby" }, { id: 2, name: "JavaScript" }, { id: 3, name: "Python" }];
+    })
+    .controller("studentsCtrl", function(studentsList, $route, $location) {
+
+        var vm = this;
+
+        vm.searchStudent = function() {
+            if(vm.name) {
+                $location.url("/studentsSearch/" + vm.name);
+            }
+            else {
+                $location.url("/studentsSearch");
+            }
+        }
+
+        vm.reloadData = function() {
+            $route.reload();
+        }
+
+        vm.students = studentsList;
+    }).controller("studentDetailsCtrl", function($http, $routeParams) {
+        var vm = this;
+        // $http({
+        //   url: "StudentsService/GetStudent",
+        //   params: {id:$routeParams.id},
+        //   method: "get"
+        // })
+        // .then(function(response) {
+        //   vm.student = response.data;
+        // }, function(reason) {
+        //   vm.error = reason.data;
+        // });
+        var students = [{ id: 1, name: "Ben", gender: "Male", city: "London" }, { id: 2, name: "Matt", gender: "Male", city: "New York" }, { id: 3, name: "Pam", gender: "Female", city: "Chennai" }];
+        vm.student = students[$routeParams.id-1];
+    }).controller("studentsSearchCtrl", function($http, $routeParams, $log) {
+        var vm = this;
+        // Check is a 'name' parameters is in the URL
+        if($routeParams.name) {
+            // $http({
+            //   url: "StudentsService/GetStudentsByName",
+            //   params: {name: $routeParams.name},
+            //   method: "get"
+            // })
+            // .then(function(response) {
+            //   vm.student = response.data;
+            // }, function(reason) {
+            //   vm.error = reason.data
+            // });
+            var students = [{ id: 1, name: "Ben", gender: "Male", city: "London" }, { id: 2, name: "Matt", gender: "Male", city: "New York" }, { id: 3, name: "Pam", gender: "Female", city: "Chennai" }];
+            // Use a filter method to retrieve only those students whose name match with the name provided in the URL
+            function filterStudentsByName(student) {
+                $log.debug(student);
+                // Check if the name in the param is a substring of the student name
+                return student.name.toUpperCase().indexOf($routeParams.name.toUpperCase()) >= 0;
+            }
+            var matchingStudents = students.filter(filterStudentsByName);
+            $log.debug(matchingStudents);
+            vm.students = matchingStudents;
+        }
+        else {
+            // $http.get("StudentsService/GetAllStudents")
+            //      .then(function(response) {
+            //            vm.students = response.data;
+            //       }, function(reason) {
+            //            vm.error = reason.data;
+            //       });
+            vm.students = [{ id: 1, name: "Ben", gender: "Male", city: "London" }, { id: 2, name: "Matt", gender: "Male", city: "New York" }, { id: 3, name: "Pam", gender: "Female", city: "Chennai" }];
+        }
+    });
+
